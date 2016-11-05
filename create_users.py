fafe63a3a5b2f293dbe6001beaa63a45129be29a
IARyan <ryan.grandgenett@gmail.com>
2016-11-05 14:13:35 -0500

    Use Flask sqlalchemy

--- create_users.py
+++ create_users.py
@@ -1,14 +1,66 @@
 """
-Temp Script for creating users in the database
+Temp Script to create database with initialized data
 """
+from server import db
+from server import User
+import os
 
-from sql.sql import SQL
+# Remove database file is already exists
+try:
+	if os.stat("server.db"):
+	   os.remove("server.db")
+except:
+	"Print server.db not found"	
 
-db = SQL()
+# Create new database and add users
+db.create_all()
 
-db.add_user(username="josiah", password="test123", is_child=0, user_stage=0, full_name="Josiah South")
-db.add_user(username="adam", password="adam", is_child=0, user_stage=0, full_name="Adam Schaal")
-db.add_user(username="ryan", password="ryan", is_child=0, user_stage=0, full_name="Ryan Grandgenett")
-db.add_user(username="child1", password="child", is_child=1, user_stage=1, full_name="Evan Schaal")
-db.add_user(username="child2", password="child", is_child=1, user_stage=2, full_name="Evan Schaal")
-db.add_user(username="child3", password="child", is_child=1, user_stage=3, full_name="Evan Schaal")
+# Create child objects
+maggie = User(username="maggie", password="maggie", is_child=1, user_stage=2, full_name="Maggie Simpson", children=[])
+lisa = User(username="lisa", password="lisa", is_child=1, user_stage=2, full_name="Lisa Simpson", children=[])
+bart = User(username="bart", password="bart", is_child=1, user_stage=3, full_name="Bart Simpson", children=[])
+evan = User(username="child1", password="child", is_child=1, user_stage=1, full_name="Evan Schaal", children=[])
+future1 = User(username="future1", password="future1", is_child=1, user_stage=2, full_name="Future Grandgenett", children=[])
+future2 = User(username="future2", password="future2", is_child=1, user_stage=3, full_name="Future South", children=[])
+
+# Create parent objects
+ryan = User(username="ryan", password="ryan", is_child=0, user_stage=0, full_name="Ryan Grandgenett", children=[future1])
+adam = User(username="adam", password="adam", is_child=0, user_stage=0, full_name="Adam Schaal", children=[evan])
+josiah = User(username="josiah", password="test123", is_child=0, user_stage=0, full_name="Josiah South", children=[future2])
+homer = User(username="homer", password="homer", is_child=0, user_stage=0, full_name="Homer Simpson", children=[bart, maggie, lisa])
+marge = User(username="marge", password="marge", is_child=0, user_stage=0, full_name="Marge Simpson", children=[bart, maggie, lisa])
+
+# Add the objects to the database
+db.session.add(maggie)
+db.session.add(lisa)
+db.session.add(bart)
+db.session.add(evan)
+db.session.add(adam)
+db.session.add(future1)
+db.session.add(future2)
+db.session.add(ryan)
+db.session.add(adam)
+db.session.add(josiah)
+db.session.add(homer)
+db.session.add(marge)
+db.session.commit()
+
+parents = User.query.filter_by(is_child=0)
+children = User.query.filter_by(is_child=1)
+
+print "\n\n===Parents of children in the database===\n"
+for parent in parents:
+	print "{0}:".format(parent.full_name)
+	for child in parent.children:
+		print "\t{0}".format(child.full_name)
+	print "\n"
+
+print "\n\n===Children of parents in the database===\n"
+for child in children:
+	print "{0}:".format(child.full_name)
+	for parent in child.parents:
+		print "\t{0}".format(parent.full_name)
+	print "\n"
+
+user = User.query.filter_by(username="wrong").first()
+print user
--- server.py
+++ server.py
@@ -1,24 +1,62 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS, cross_origin
-from sql.sql import SQL
-import json
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///server.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 CORS(app)
 api = Api(app)
-db = SQL()
 
+
+#Table to handle the self-referencing many-to-many relationship for the User class:
+#First column holds the user who is a parent, the second the user is the child.
+user_to_user = db.Table('user_to_user',
+    db.Column("parent_id", db.Integer, db.ForeignKey("user.id"), primary_key=True),
+    db.Column("child_id", db.Integer, db.ForeignKey("user.id"), primary_key=True)
+)
+
+
+class User(db.Model):
+  __tablename__ = 'user'
+  id = db.Column(db.Integer, primary_key=True)
+  #parent_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+  username = db.Column(db.String(250), unique=True)
+  password = db.Column(db.String(250))
+  is_child = db.Column(db.Integer)
+  user_stage =  db.Column(db.Integer)
+  full_name = db.Column(db.String(250))
+  children = db.relationship("User",
+                    secondary=user_to_user,
+                    primaryjoin=id==user_to_user.c.parent_id,
+                    secondaryjoin=id==user_to_user.c.child_id,
+                    backref="parents"
+    )
+
+  def __init__(self, username, password, is_child, user_stage, full_name, children):
+    self.username = username
+    self.password = password
+    self.is_child = is_child
+    self.user_stage = user_stage
+    self.full_name = full_name
+    self.children = children
+
+    
 # Check login credentials
 class Login(Resource):
     def post(self):
       # Receive the login data
       login_data = request.get_json()
-      input_username =(login_data["username"]).lower()
-      input_password = login_data["password"]
-      if db.valid_user(input_username):
-        user = db.get_user(input_username)
-        if user.password == input_password:
+      # Pull out json data
+      json_username =(login_data["username"]).lower()
+      json_password = login_data["password"]
+
+      # Look for user in the database
+      user = User.query.filter_by(username=json_username).first()
+      if user:
+        if user.password == json_password:
           if user.is_child:
             return {"username": user.username, "isChild": user.is_child, "userStage": user.user_stage, "fullName": user.full_name} 
           else:
