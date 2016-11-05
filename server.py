661c96202537d9e1d71b65be2d1afe4487d3568a
IARyan <ryan.grandgenett@gmail.com>
2016-11-05 03:41:08 -0500

    Update Login Failed Error Message

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ class Login(Resource):
             return {"username": user.username, "isChild": user.is_child, "userStage": user.user_stage, "fullName": user.full_name} 
           else:
              return {"username": user.username, "isChild": user.is_child, "fullName": user.full_name} 
-      return {"status":"Login Failed :'("}
+      return {"status":"Login Failed :'(  ¯\_(ツ)_/¯"}
 
 
 api.add_resource(Login, '/login')
