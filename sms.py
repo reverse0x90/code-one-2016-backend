e7bd408cf1800794f025df17caa6cbeed8e48df1
IARyan <ryan.grandgenett@gmail.com>
2016-11-06 11:55:46 -0600

    Huh

--- sms.py
+++ sms.py
@@ -14,13 +14,14 @@ def reply_payment():
 
     if "Approve" or "Approve" in message_body:
          # Save and update the chore status
-        with open('chore_state.p', 'rb') as pickle_file:
-            status_stack = pickle.load(pickle_file)
-
+        pickle_file = open('chore_state.p', 'rb') 
+        status_stack = pickle.load(pickle_file)
         chore_vars = status_stack.pop()
+        pickle_file.close()
 
-        with open('chore_state.p', 'wb') as pickle_file:
-            pickle.dump(status_stack, pickle_file)
+        pickle_file = open('chore_state.p', 'wb')
+        pickle.dump(status_stack, pickle_file)
+        pickle_file.close()
 
         chore_vars["status"] = "completed"
         payload = {"username": chore_vars["username"], "title": chore_vars["title"], "status": chore_vars["status"]}
@@ -31,13 +32,15 @@ def reply_payment():
 
     elif "Deny" or "deny" in message_body:
         # Save and update the chore status
-        with open('chore_state.p', 'rb') as pickle_file:
-            status_stack = pickle.load(pickle_file)
+        pickle_file = open('chore_state.p', 'rb')
+        status_stack = pickle.load(pickle_file)
+        pickle_file.close()
 
         chore_vars = status_stack.pop()
 
-        with open('chore_state.p', 'wb') as pickle_file:
-            pickle.dump(status_stack, pickle_file)
+        pickle_file = open('chore_state.p', 'wb')
+        pickle.dump(status_stack, pickle_file)
+        pickle_file.close()
 
         chore_vars["status"] = "not-completed"
         payload = {"username": chore_vars["username"], "title": chore_vars["title"], "status": chore_vars["status"]}
@@ -47,12 +50,14 @@ def reply_payment():
         reply_message = "Thank you for using First National Bank your chore payment for %s completing the chore %s has been denied and no funds have been transfered." % (chore_vars["username"].title(), chore_vars["title"])
     else:
         # Save and update the chore status
-        with open('chore_state.p', 'rb') as pickle_file:
-            status_stack = pickle.load(pickle_file)
-
+        pickle_file = open('chore_state.p', 'rb')
+        status_stack = pickle.load(pickle_file)
+        pickle_file.close()
+        
         chore_vars = status_stack.peek()
 
         reply_message = "I am sorry I didn't understand your response. Please reply Approve or Deny the requested payment for %s completing the chore %s." % (chore_vars["username"].title(), chore_vars["title"])
+    
     resp = twilio.twiml.Response()
     resp.message(reply_message)
 
