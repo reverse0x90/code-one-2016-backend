c62f65112c6e9c65fdce01b795527393340fd2d8
IARyan <ryan.grandgenett@gmail.com>
2016-11-06 09:40:55 -0600

    sms update

--- sms.py
+++ sms.py
@@ -10,17 +10,15 @@ def reply_payment():
     message_body = request.values.get('Body', None)
 
     if "Approve" or "Approve" in message_body:
-    	reply_message = "Thank you for using First National Bank your the chore payment for Evan has has been approved."
-    else if "Deny" or "deny" in message_body:
-    	reply_message = "Thank you for using First National Bank your the chore payment for Evan has has been denied."
+        reply_message = "Thank you for using First National Bank your the chore payment for Evan has has been approved."
+    elif "Deny" or "deny" in message_body:
+        reply_message = "Thank you for using First National Bank your the chore payment for Evan has has been denied."
     else:
-    	reply_message = "I am sorry I did't understand your response. Please reply Approve or Deny."
-y
+        reply_message = "I am sorry I did't understand your response. Please reply Approve or Deny."
     resp = twilio.twiml.Response()
     resp.message(reply_message)
 
     return str(resp)
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
